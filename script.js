f8c8af0ef917707f7336f2d6ce573fb760363fe2
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-10-23 11:35:17 +0100

    e-commece application version 1.0

---
+++ script.js
@@ -0,0 +1,59 @@
+var productArray = [];
+
+
+function submitAll(){
+  var product = productName.value;
+  var description = descriptionName.value;
+  var price = priceAmt.value;
+  var commerceObj = {product,description, price}
+  console.log(commerceObj);
+  if (product == "" || description == "" || price == ""){
+    showErr.innerHTML = `<p class="alert alert-danger text-center fw-bold">all inputs must be filled</p>`
+  }else{
+    showErr.innerHTML = ""
+    productArray.push(commerceObj);
+    productName.value = ""
+    descriptionName.value = ""
+    priceAmt.value = ""
+    displayAll()
+  }
+}
+
+function deleteAny(i){
+  var check = confirm("Do you want to edit")
+
+  if(check == true){
+
+    showErr.innerHTML = ""
+    productArray.splice(i, 1)
+    displayAll()
+    showErr.innerHTML = `<p class="alert alert-success text-center fw-bold">Deleted successfully</p>`
+  }else{
+    
+  }
+}
+
+function editAny(i){
+    showErr.innerHTML = `<p class="alert alert-success text-center fw-bold">Edit successfully</p>`
+    productArray[i]["product"] = productName.value;
+    productArray[i]["description"] = descriptionName.value;
+    productArray[i]["price"] = priceAmt.value;
+    displayAll()
+  }
+
+
+
+function displayAll(){
+  display.innerHTML = ""
+  productArray.map((element, i)=>{
+    display.innerHTML += `<tr>
+    <td>${i+1}</td>
+    <td>${element.product}</td>
+    <td>${element.description}</td>
+    <td>${element.price}</td>
+    <td><button onclick="deleteAny(${i})" class="btn btn-success">Delete</button></td>
+    <td><button onclick="editAny(${i})" class="btn btn-warning">Edit</button></td>
+
+    </tr>`
+  })
+}
